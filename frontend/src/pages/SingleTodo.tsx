8ba8370bfb472bc4d91f5785657e355e41b18891
Zaid Malik <mzaidmalik25@gmail.com>
2026-03-24 21:56:45 +0530

    show single todo

--- frontend/src/pages/SingleTodo.tsx
+++ frontend/src/pages/SingleTodo.tsx
@@ -1,37 +1,47 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import type { ApiResponse } from "../types/api.types";
+import type { todoI } from "../types/todo.types";
 
 const SingleTodo = () => {
-  // temporary dummy data (baad me API se aayega)
-  const [todo] = useState({
-    title: "Learn TypeScript",
-    description: "Practice TS with React and backend",
-    isCompleted: false,
-  });
+  const { id } = useParams()
 
+  const [todo, setTodo] = useState<todoI | null >(null);
+
+  useEffect(() => {
+    const getSingleTodo = async () => {
+      const res = await axios.get<ApiResponse<todoI>>(`http://localhost:3000/api/todo/${id}`, {
+        withCredentials: true
+      })
+      setTodo(res.data.data)
+    }
+    getSingleTodo()
+  } , [id])
   return (
     <div className="min-h-screen bg-gray-900 flex items-center justify-center">
       <div className="bg-gray-800 p-8 rounded-xl shadow-lg w-full max-w-lg">
 
         {/* Title */}
         <h2 className="text-2xl font-bold text-white mb-4">
-          {todo.title}
+          {todo?.title}
         </h2>
 
         {/* Description */}
         <p className="text-gray-300 mb-6">
-          {todo.description}
+          {todo?.description}
         </p>
 
         {/* Status */}
         <div className="mb-6">
           <span
             className={`px-3 py-1 rounded text-sm ${
-              todo.isCompleted
+              todo?.isCompleted
                 ? "bg-green-500 text-white"
                 : "bg-yellow-500 text-black"
             }`}
           >
-            {todo.isCompleted ? "Completed" : "Pending"}
+            {todo?.isCompleted ? "Completed" : "Pending"}
           </span>
         </div>
 
